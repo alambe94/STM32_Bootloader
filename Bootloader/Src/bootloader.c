be23fa0fe808732cf5f97decd119b67182500587
alambe94 <lambealsaad@gmail.com>
2019-10-18 19:08:17 +0530

    initial commit

---
+++ Bootloader/Src/bootloader.c
@@ -0,0 +1,404 @@
+/*
+ * bootloader.c
+ *
+ *  Created on: Sep 25, 2019
+ *      Author: medprime
+ */
+
+
+#include "bootloader.h"
+#include "stm32f4xx_hal.h"
+#include "string.h"
+#include "stdlib.h"
+#include "printf.h"
+
+
+/**
+ *  STM32F401RE -- 512KB total flash.
+ *  Bootloader resides in sector 0,1 -- 16KB + 16KB Flash.
+ *  Total sectors in 401RE are 8.
+ *  Sectors to erase 6, except sector 0,1 (bootloader).
+ *  Erase type- sector by sector.
+ *  Programaing voltage- 2.7v to 3.3v.
+ *  Flash writing width - byte by byte for 2.7v to 3.3v.
+ */
+
+#define BL_TOTAL_SECTORS 8 // 16KB+16KB+16KB+16KB+64KB+128KB+128KB+128KB
+#define BL_USED_SECTORS  2 // 16KB + 16KB
+
+#define USER_FLASH_START_ADDRESS (0x08008000)
+#define USER_FLASH_END_ADDRESS   (0x08008000 + 480000) // 32KB used by bootloader remaing 48K
+
+#define CMD_WRITE  0x50
+#define CMD_READ   0x51
+#define CMD_ERASE  0x52
+#define CMD_RESET  0x53
+#define CMD_JUMP   0x54
+#define CMD_VERIFY 0x55
+
+
+#define CMD_ACK    0x90
+#define CMD_NACK   0x91
+#define CMD_ERROR  0x92
+
+#define CMD_HELP   0x40
+
+#define SYNC_CHAR  '$'
+
+#define  RX_BUFFER_SIZE   (256)
+uint8_t  RX_Buffer[RX_BUFFER_SIZE];
+
+extern CRC_HandleTypeDef hcrc;
+extern UART_HandleTypeDef huart2;
+
+UART_HandleTypeDef* BL_UART = &huart2;
+
+
+static uint32_t Bootloader_Timeout = 3000;  // 3 seconds
+
+
+/*Maxim APPLICATION NOTE 27 */
+uint8_t CRC8_Table[] =
+    {
+	0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,
+	157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,
+	35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,
+	190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,
+	70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,
+	219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,
+	101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,
+	248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,
+	140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,
+	17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,
+	175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,
+	50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,
+	202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139,
+	87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,
+	233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,
+	116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53
+    };
+
+uint8_t CRC8(uint8_t* data, uint8_t len)
+    {
+
+    uint8_t crc = 0;
+
+    for(uint8_t i=0; i<len; i++)
+	{
+	crc = CRC8_Table[crc^ data[i]];
+	}
+
+    return crc;
+    }
+
+
+
+void BL_UART_Send_Char(char data)
+    {
+    BL_UART->Instance->DR = data;
+    while (__HAL_UART_GET_FLAG(BL_UART,UART_FLAG_TC) == 0);
+    }
+
+void BL_UART_Send_String(char* data)
+    {
+
+    uint16_t len = strlen(data);
+
+    while (len--)
+	{
+	BL_UART_Send_Char(*data);
+	data++;
+	}
+
+    }
+
+void Write_Callback(uint32_t address, const uint8_t *data, uint8_t len)
+    {
+
+    uint8_t status = 1;
+
+    if (address >= USER_FLASH_START_ADDRESS
+	    && address <= USER_FLASH_END_ADDRESS - len)
+	{
+
+	/* Unlock the Flash to enable the flash control register access *************/
+	HAL_FLASH_Unlock();
+
+	for (uint8_t i = 0; i < len; i++)
+	    {
+
+	    if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE, address,
+		    (uint64_t) *data) == HAL_OK)
+		{
+		/* Check the written value */
+		if (*(uint8_t*) address != (*data))
+		    {
+		    /* Flash content doesn't match SRAM content */
+		    status = 0;
+		    break;
+		    }
+		/* Increment FLASH destination address */
+		address++;
+		data++;
+		}
+	    else
+		{
+		/* Error occurred while writing data in Flash memory */
+		status = 0;
+		break;
+		}
+	    }
+
+	/* Lock the Flash to disable the flash control register access (recommended
+	 to protect the FLASH memory against possible unwanted operation) *********/
+	HAL_FLASH_Lock();
+
+	if (status)
+	    {
+	    BL_UART_Send_Char(CMD_ACK);
+	    }
+	else
+	    {
+	    BL_UART_Send_Char(CMD_NACK);
+	    }
+	}
+    else
+	{
+	BL_UART_Send_Char(CMD_NACK);
+	}
+
+    }
+
+void Read_Callback(uint32_t address, uint8_t len)
+    {
+    // reading flash
+    }
+
+void Erase_Callback()
+    {
+
+    if (BL_Erase_Flash())
+	{
+	BL_UART_Send_Char(CMD_ACK);
+	}
+    else
+	{
+	BL_UART_Send_Char(CMD_NACK);
+	}
+    }
+
+void Reset_Callback()
+    {
+    // reset mcu
+    HAL_NVIC_SystemReset();
+    }
+
+
+typedef  void (*pFunction)(void);
+
+pFunction Jump_To_App;
+
+void Jump_Callback()
+    {
+
+    uint32_t app_adress = 0;
+
+    BL_UART_Send_Char(CMD_ACK);
+
+    HAL_DeInit();
+
+    /* execute the new program */
+    app_adress = *(__IO uint32_t*) (USER_FLASH_START_ADDRESS + 4);
+
+    /* Jump to user application */
+    Jump_To_App = (pFunction) app_adress;
+
+    /* Initialize user application's Stack Pointer */
+    __set_MSP(*(__IO uint32_t*) USER_FLASH_START_ADDRESS);
+
+    Jump_To_App();
+
+    }
+
+
+void Verify_Callback(uint32_t address, const uint8_t* data, uint8_t len)
+    {
+    // verify flash
+    }
+
+
+void Help_Callback()
+    {
+
+    }
+
+uint8_t BL_Erase_Flash()
+    {
+
+    uint8_t status = 0;
+    uint32_t error = 0;
+
+    FLASH_EraseInitTypeDef flash_erase_handle;
+
+    flash_erase_handle.TypeErase = FLASH_TYPEERASE_SECTORS;
+    flash_erase_handle.Banks = FLASH_BANK_1;
+    flash_erase_handle.Sector = BL_USED_SECTORS;
+    flash_erase_handle.NbSectors = 6;
+    flash_erase_handle.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+
+    HAL_FLASH_Unlock();
+
+    if (HAL_FLASHEx_Erase(&flash_erase_handle, &error) == HAL_OK)
+	{
+	status = 1;
+	}
+
+    HAL_FLASH_Lock();
+
+    return status;
+    }
+
+
+void Bootloader()
+    {
+
+    uint32_t write_address = 0;
+    uint8_t write_len = 0;
+
+    uint32_t read_address = 0;
+    uint8_t read_len = 0;
+
+    uint32_t verify_address = 0;
+    uint8_t verify_len = 0;
+
+    while (1)
+	{
+
+	memset(RX_Buffer, 0x00, RX_BUFFER_SIZE);
+
+	/* wait for sync char*/
+	if (HAL_UART_Receive(BL_UART, RX_Buffer, 1, HAL_MAX_DELAY)
+		== HAL_OK)
+	    {
+
+	    uint8_t sync_char = RX_Buffer[0];
+
+	    if (sync_char == SYNC_CHAR)
+		{
+
+		/* wait for packet len char*/
+		if (HAL_UART_Receive(BL_UART, RX_Buffer, 1, 100) == HAL_OK)
+		    {
+
+		    uint8_t packet_len = RX_Buffer[0];
+
+		    if (HAL_UART_Receive(BL_UART, RX_Buffer, packet_len,
+			    (packet_len + 100)) == HAL_OK)
+			{
+
+			uint8_t cmd = RX_Buffer[0];
+
+			/* last byte is crc*/
+			uint8_t crc_recvd = RX_Buffer[packet_len - 1];
+
+			/* calculate crc */
+			uint8_t crc_calc = CRC8(RX_Buffer, (packet_len - 1));
+
+			if (crc_calc == crc_recvd)
+			    {
+
+			    switch (cmd)
+				{
+
+			    case CMD_WRITE:
+
+				write_address = RX_Buffer[1] << 24|
+				                RX_Buffer[2] << 16|
+				                RX_Buffer[3] << 8|
+				                RX_Buffer[4] << 0;
+
+				write_len = RX_Buffer[5]; // number of bytes to write
+
+				Write_Callback(write_address, (RX_Buffer + 6),
+					write_len);
+				break;
+
+			    case CMD_READ:
+
+				read_address = RX_Buffer[1] << 24
+					| RX_Buffer[2] << 16 | RX_Buffer[3] << 8
+					| RX_Buffer[4] << 0;
+
+				read_len = RX_Buffer[5];
+
+				Read_Callback(read_address, read_len);
+				break;
+
+			    case CMD_ERASE:
+				Erase_Callback();
+				break;
+
+			    case CMD_RESET:
+				Reset_Callback();
+				break;
+
+			    case CMD_JUMP:
+				Jump_Callback();
+				break;
+
+			    case CMD_VERIFY:
+
+				verify_address = RX_Buffer[1] << 24
+					| RX_Buffer[2] << 16 | RX_Buffer[3] << 8
+					| RX_Buffer[4] << 0;
+
+				verify_len = RX_Buffer[5];
+
+				Verify_Callback(verify_address, (RX_Buffer + 6),
+					verify_len);
+				break;
+
+			    case CMD_HELP:
+				Help_Callback();
+				break;
+
+			    default:
+				//print CMD_ERROR
+				break;
+				}
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+
+
+void BL_Main_Loop()
+    {
+
+    HAL_Delay(1);
+
+    BL_UART_Send_Char(CMD_ACK);
+
+    /* wait for ack to enter bootloader char*/
+    if (HAL_UART_Receive(BL_UART, RX_Buffer, 1, Bootloader_Timeout) == HAL_OK)
+	{
+
+	if(RX_Buffer[0] == CMD_ACK)
+	    {
+		Bootloader();
+		Reset_Callback();
+	    }
+	}
+    else
+	{
+	Jump_Callback();
+	}
+    }
+
+void BL_UART_RX_ISR()
+    {
+
+    }
---
+++ Bootloader/python/stm32_bootloader.py
@@ -0,0 +1,251 @@
+import serial
+import struct
+import os
+import sys
+import time
+
+
+CMD_WRITE = 0x50
+CMD_READ = 0x51
+CMD_ERASE = 0x52
+CMD_RESET = 0x53
+CMD_JUMP = 0x54
+CMD_VERIFY = 0x55
+
+CMD_ACK = 0x90
+CMD_NACK = 0x91
+CMD_ERROR = 0x92
+
+CMD_HELP  = 0x40
+
+SYNC_CHAR  = ord('$')
+
+
+# Maxim APPLICATION NOTE 27 
+
+CRC8_Table = [
+	0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,
+	157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,
+	35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,
+	190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,
+	70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,
+	219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,
+	101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,
+	248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,
+	140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,
+	17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,
+	175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,
+	50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,
+	202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139,
+	87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,
+	233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,
+	116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53
+ ]
+
+def CRC8(data , len):
+    
+    crc = 0
+    for i in range(len):
+        crc = CRC8_Table[crc ^ data[i]]
+        
+    return crc
+
+def int_to_byte(data):
+    return bytes([data])
+ 
+def stm32_bl_send_cmd(cmd):
+         
+    crc = CRC8([cmd], 1)
+    
+    packet = bytes()
+    packet += int_to_byte(SYNC_CHAR)
+    packet += int_to_byte(2)
+    packet += int_to_byte(cmd)
+    packet += int_to_byte(crc)
+
+    ser.write(packet)
+        
+       
+def stm32_send_ack():
+    ser.write(int_to_byte(CMD_ACK))
+
+    
+def stm32_read_line():
+    rx_string = ser.readline().decode('utf-8')
+    return rx_string
+
+def stm32_read_ack():
+    
+    rx_char = ser.read(1)
+    
+    if(rx_char == b''):
+        return CMD_NACK
+    else:
+        return ord(rx_char)
+        
+    
+
+def stm32_erase():
+    stm32_bl_send_cmd(CMD_ERASE)
+    reply = stm32_read_ack()
+    if(reply == CMD_ACK):
+        print('flash erase success')
+    elif (reply == CMD_NACK):
+        print('flash erase error')
+
+def stm32_enter_application():
+    stm32_bl_send_cmd(CMD_JUMP)
+    reply = stm32_read_ack()
+    if(reply == CMD_ACK):
+        print('entering user application')
+    elif (reply == CMD_NACK):
+        print('user application failed')
+
+def stm32_get_help():
+    print("""
+       supported commands
+       write  -> write application to mcu.
+       erase  -> erase mcu flash.
+       reset  -> reset mcu.
+       jump   -> jump to user application.
+       read   -> read flash from mcu.
+       verify -> verify mcu content.
+       """)
+
+def stm32_reset():
+    stm32_bl_send_cmd(CMD_RESET)
+    reply = stm32_read_ack()
+    if(reply == CMD_ACK):
+        print('mcu reset')
+    elif (reply == CMD_NACK):
+        print('mcu reset failed')
+
+
+def stm32_jump():
+    stm32_bl_send_cmd(CMD_JUMP)
+    reply = stm32_read_ack()
+    if(reply == CMD_ACK):
+        print('jumping to user apllication')
+    elif (reply == CMD_NACK):
+        print('ump to user apllication failed')
+
+
+def stm32_write():
+
+    f_file_len = 0
+    f_file_exist = False
+    data_len = 128+64+32
+    stm32_app_address = 0x08008000
+
+    
+
+    try:
+        f_file_len = os.path.getsize(bin_file)
+        print("file size " + str(f_file_len))
+        bin_file_data = open(bin_file,'rb')
+        f_file_exist = True
+    except(OSError):
+        print("can not open " + bin_file)
+
+    while(f_file_len >0):
+        
+
+        if(f_file_len < data_len):
+            data_len = f_file_len
+
+        bl_packet = bytes()
+        # payload structure 1-byte cmd + 4-byte addes + 1-byte data size + payload + 1-byte CRC
+        
+        #assemble cmd
+        bl_packet += int_to_byte(CMD_WRITE)
+
+        #assemble address
+        bl_packet += int_to_byte(stm32_app_address >> 24 & 0xFF)
+        bl_packet += int_to_byte(stm32_app_address >> 16 & 0xFF)
+        bl_packet += int_to_byte(stm32_app_address >> 8 & 0xFF)
+        bl_packet += int_to_byte(stm32_app_address >> 0 & 0xFF)
+        
+        #assemble no of bytes to write
+        bl_packet += int_to_byte(data_len)
+
+        #assemble payload
+        for x in range(data_len):
+            bl_packet += bin_file_data.read(1)
+
+        #assemble crc
+        crc = CRC8(bl_packet, (data_len + 6))
+        
+        bl_packet += int_to_byte(crc)
+        
+        
+        ser.write(int_to_byte(SYNC_CHAR))
+        ser.write(int_to_byte(data_len + 7))
+        ser.write(bl_packet)
+        
+        reply = stm32_read_ack()
+        if(reply == CMD_ACK):
+            print('flash write success at ' + hex(stm32_app_address))
+        elif (reply == CMD_NACK):
+            print('flash write error at ' + hex(stm32_app_address))
+            break
+        
+        f_file_len -= data_len
+        stm32_app_address += data_len
+        print("remaining bytes:{}".format(f_file_len))
+        
+        if(f_file_len == 0):
+            print("flash write successfull, jolly good!!!!")
+        
+
+    if(f_file_exist):
+        bin_file_data.close()
+        print("closing file")
+
+
+
+ser_open = False
+port = 'ACM0'
+baud = 38400   
+    
+if len(sys.argv) >= 2:
+    cmd = sys.argv[1]
+    
+    try:
+        ser = serial.Serial("/dev/tty"+port, baud, timeout=5)
+        ser_open = True
+    except:
+        print("\n Not valid port")
+else:
+    print("please enter port, baud rate and cmd and optional input file")
+
+if ser_open:
+    print("Port open success")
+    
+    ack = stm32_read_ack()
+    
+    if (ack == CMD_ACK):
+        print("connected to target")
+        stm32_send_ack()
+        if(cmd == 'write'):
+            if len(sys.argv) >= 3:
+                bin_file = sys.argv[2]
+                stm32_write()
+                stm32_jump()
+            else:
+                print('please enter input file')  
+        elif(cmd == 'erase'):
+            stm32_erase()    
+        elif(cmd == 'reset'):
+            stm32_reset() 
+        elif(cmd == 'jump'):
+            stm32_jump()     
+        elif(cmd == 'help'):
+            stm32_get_help()          
+        else:
+            print("invalid cmd")
+    else:
+        print("timeout error!! could not connect to target")    
+    
+    
+else:
+    print("Port open failed")
